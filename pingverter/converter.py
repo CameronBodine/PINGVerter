de0c02720f679509d6dd765de24f4296bad7cd05
Cameron Bodine <cbodine@udel.edu>
2024-12-11 04:37:28 -0500

    Add script preamble for v 1.0.0-alpha

--- pingverter/converter.py
+++ pingverter/converter.py
@@ -1,3 +1,44 @@
+'''
+Dependency of PINGMapper: https://github.com/CameronBodine/PINGMapper
+
+Repository: https://github.com/CameronBodine/PINGVerter
+PyPi: https://pypi.org/project/pingverter/ 
+
+Developed by Cameron S. Bodine
+
+###############
+Acknowledgments
+###############
+
+None of this work would have been possible without the following repositories:
+
+PyHum: https://github.com/dbuscombe-usgs/PyHum
+SL3Reader: https://github.com/halmaia/SL3Reader
+sonarlight: https://github.com/KennethTM/sonarlight
+
+
+MIT License
+
+Copyright (c) 2024 Cameron S. Bodine
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
 
 import os, sys
 from pingverter import hum, low
